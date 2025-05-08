2a2aa305ff2e1d5f71fc1826255468ebbf63ff83
ZhenyuWu323 <wzy2575765661@gmail.com>
2025-05-08 02:39:31 +0000

    low body only

--- source/g1_humanoid/g1_humanoid/tasks/locomotion/velocity/__init__.py
+++ source/g1_humanoid/g1_humanoid/tasks/locomotion/velocity/__init__.py
@@ -20,10 +20,20 @@ from . import agents
 # Register Gym environments.
 ##
 
+""" G1 FTP Low Body Locomotion Flat """
+
+gym.register(
+    id="Template-G1-FTP-LowBody-Locomotion-Velocity-Flat-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.g1_ftp_low_env_cfg:G1FTPLowFlatEnvCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:G1FlatPPORunnerCfg",
+    },
+)
 
 
-
-""" G1 FTP Locomotion Flat """
+""" G1 FTP Whole Body Locomotion Flat """
 
 gym.register(
     id="Template-G1-FTP-Locomotion-Velocity-Flat-v0",
